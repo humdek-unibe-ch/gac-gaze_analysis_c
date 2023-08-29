420ba58be92e60e7a31c57d6a0d3821fb73d09c8
Simon Maurer <moirelein@gmail.com>
2023-08-29 22:04:18 +0200

    first (untested) steps with AOI functions

--- include/gac.h
+++ include/gac.h
@@ -1,12 +1,14 @@
 /**
- * @file gac.h
- * @author  Simon Maurer
+ * Gaze analysis library for fixation and saccade detection in raw gaze data.
+ *
+ * @file
+ *  gac.h
+ * @author
+ *  Simon Maurer
  * @license
  *  This Source Code Form is subject to the terms of the Mozilla Public
  *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
  *  You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * Gaze analysis library for fixation and saccade detection in raw gaze data.
  */
 
 #ifndef GAC_H
@@ -18,8 +20,12 @@
 #include <cglm/mat4.h>
 #include <sys/time.h>
 
+#define GAC_AOI_MAX_POINTS 100
+
 /** ::gac_s */
 typedef struct gac_s gac_t;
+/** ::gac_aoi_s */
+typedef struct gac_aoi_s gac_aoi_t;
 /** ::gac_sample_s */
 typedef struct gac_sample_s gac_sample_t;
 /** ::gac_filter_gap_s */
@@ -46,6 +52,23 @@ typedef struct gac_saccade_s gac_saccade_t;
 typedef struct gac_screen_s gac_screen_t;
 
 /**
+ * The order of point triplets. This is used for checking
+ * whetehr a point lies within an AOI.
+ */
+enum gac_aoi_orientation_e
+{
+    /** Points are colinear. */
+    GAC_AOI_ORIENTATION_COLINEAR,
+    /** Points are ordered clockwise. */
+    GAC_AOI_ORIENTATION_CLOCKWISE,
+    /** Points are ordered counter clockwise. */
+    GAC_AOI_ORIENTATION_COUNTER_CLOCKWISE,
+};
+
+/** #gac_aoi_orientation_e */
+typedef enum gac_aoi_orientation_e gac_aoi_orientation_t;
+
+/**
  * The available noise filter types
  */
 enum gac_filter_noise_type_e
@@ -58,6 +81,26 @@ enum gac_filter_noise_type_e
 
 /** #gac_filter_noise_type_e */
 typedef enum gac_filter_noise_type_e gac_filter_noise_type_t;
+
+/**
+ * An area of interest (AOI) structure.
+ */
+struct gac_aoi_s
+{
+    /** Self-pointer to allocated structure for memory management. */ 
+    void* _me;
+    /**
+     * The points forming the AOI. At least 3 points are required for a
+     * valid AOI.
+     */
+    vec2 points[GAC_AOI_MAX_POINTS];
+    /** An arbitary point outside the AOI. */
+    vec2 ray_origin;
+    /** The average length of an AOI edge. */
+    float avg_edge_len;
+    /** The number of points defining the AOI. */
+    uint32_t count;
+};
 
 /**
  * The gaze data sample.
@@ -396,8 +439,8 @@ bool gac_get_filter_parameter( gac_t* h, gac_filter_parameter_t* parameter );
 bool gac_get_filter_parameter_default( gac_filter_parameter_t* parameter );
 
 /**
- * Configure the screen position in 3d space. This allows to compute 2d
- * gaze point coordinates.
+ * Configure the screen position in 3d space. This allows to compute normalized
+ * 2d gaze point coordinates.
  *
  * @param h
  *  A pointer to the gaze analysis handler.
@@ -426,6 +469,116 @@ bool gac_set_screen( gac_t* h,
         float top_left_x, float top_left_y, float top_left_z,
         float top_right_x, float top_right_y, float top_right_z,
         float bottom_left_x, float bottom_left_y, float bottom_left_z );
+
+// AOI /////////////////////////////////////////////////////////////////////////
+
+/**
+ * Add a point the AOE definition. An AOE requires at least 3 points to be
+ * valid. In addition to attaching the point to the internal array, this
+ * function computes a point which is guaranteed to be outside of the AOI at
+ * a reasonable distance from the AOI.
+ *
+ * @param aoi
+ *  A pointer to an aoi structure.
+ * @param x
+ *  The normalised x coordinate of the AOI point to add.
+ * @param y
+ *  The normalised y coordinate of the AOI point to add.
+ * @return
+ *  True on success, false on failure.
+ */
+bool gac_aoi_add_point( gac_aoi_t* aoi, float x, float y );
+
+/**
+ * Allocate a new AOI structure. This must be freed with gac_aoi_destroy().
+ *
+ * @return
+ *  A pointer to the allocated structure or NULL on failure.
+ */
+gac_aoi_t* gac_aoi_create();
+
+/**
+ * Destroies a AOI structure. This works for structures created with
+ * gac_aoi_create() as well as gac_aoi_init().
+ *
+ * @param aoi
+ *  A pointer to a AOI structure to destroy.
+ */
+void gac_aoi_destroy( gac_aoi_t* aoi );
+
+/**
+ * Checks whether a point is inside of an AOI. This function uses the ray
+ * casting method where a virtual ray is drawn from an arbitraty point outside
+ * the AOI to the point. Then, every intersection with segments of the AOI
+ * contour is counted.  If an even number of intersection is detected, the
+ * point lies outside of the AOI, otherwise the point lies inside the AOI.
+ *
+ * @param aoi
+ *  A pointer to an AOI structure.
+ * @param x
+ *  The normalised x coordinate of the point to check.
+ * @param y
+ *  The normalised y coordinate of the point to check.
+ * @return
+ *  True if the point is inside the AOI, false otherwise.
+ */
+bool gac_aoi_includes_point( gac_aoi_t* aoi, float x, float y );
+
+/**
+ * Initialise the AOI structure.
+ *
+ * @param aoi
+ *  A pointer to the aoi structure to initialise.
+ * @return
+ *  True on success, false otherwise.
+ */
+bool gac_aoi_init( gac_aoi_t* aoi );
+
+/**
+ * Checks whether the line segment p1q1 intersects with the line segment p2q2.
+ *
+ * @param p1
+ *  A pointer to the staring point of the first segment.
+ * @param q1
+ *  A pointer to the end point of the first segment.
+ * @param p2
+ *  A pointer to the staring point of the second segment.
+ * @param q2
+ *  A pointer to the end point of the second segment.
+ * @return
+ *  True if the two segments intersect, false otherwise.
+ */
+bool gac_aoi_intersect( vec2* p1, vec2* q1, vec2* p2, vec2* q2 );
+
+/**
+ * Given three colinear points, this function checks if a point p lies on a
+ * segment s1s2.
+ *
+ * @param p
+ *  A pointer to the point to check.
+ * @parma s1
+ *  The starting point of the segment.
+ * @param s2
+ *  The end point of the segment.
+ * @return
+ *  True if the point lies on the segment, false otherwise.
+ */
+bool gac_aoi_point_on_segment( vec2* p, vec2* s1, vec2* s2 );
+
+/**
+ * Given three ordered points p, q, and r, this function detects whether the
+ * points are colinear, ordered clockwise or counter clockwise.
+ *
+ * @param p
+ *  A pointer to point p.
+ * @param q
+ *  A pointer to point q.
+ * @param r
+ *  A pointer to point r.
+ * @return
+ *  The orientation of the three points.
+ */
+gac_aoi_orientation_t gac_aoi_orientation_triplet( vec2* p, vec2* q, vec2* r );
 
 // FILTER //////////////////////////////////////////////////////////////////////
 
--- src/gac.c
+++ src/gac.c
@@ -165,6 +165,205 @@ bool gac_set_screen( gac_t* h,
 }
 
 /******************************************************************************/
+bool gac_aoi_add_point( gac_aoi_t* aoi, float x, float y )
+{
+    float distance;
+
+    if( aoi == NULL || aoi->count == GAC_AOI_MAX_POINTS )
+    {
+        return false;
+    }
+
+    aoi->points[aoi->count][0] = x;
+    aoi->points[aoi->count][1] = y;
+
+    if( aoi->count > 0 )
+    {
+        distance = glm_vec2_distance( aoi->points[aoi->count],
+                aoi->points[aoi->count - 1] );
+        aoi->avg_edge_len += ( distance - aoi->avg_edge_len ) / aoi->count;
+    }
+
+    aoi->count++;
+
+    if( x < aoi->ray_origin[0] )
+    {
+        aoi->ray_origin[0] = x - aoi->avg_edge_len;
+    }
+
+    if( y < aoi->ray_origin[1] )
+    {
+        aoi->ray_origin[1] = y - aoi->avg_edge_len;
+    }
+
+    return true;
+}
+
+/******************************************************************************/
+gac_aoi_t* gac_aoi_create()
+{
+    gac_aoi_t* aoi = malloc( sizeof( gac_aoi_t ) );
+
+    if( !gac_aoi_init( aoi ) )
+    {
+        return NULL;
+    }
+    aoi->_me = aoi;
+
+    return aoi;
+}
+
+/******************************************************************************/
+void gac_aoi_destroy( gac_aoi_t* aoi )
+{
+    if( aoi == NULL )
+    {
+        return;
+    }
+
+    if( aoi->_me != NULL )
+    {
+        free( aoi->_me );
+    }
+}
+
+/******************************************************************************/
+bool gac_aoi_includes_point( gac_aoi_t* aoi, float x, float y )
+{
+    uint32_t i;
+    uint32_t count = 0;
+    vec2 point = { x, y };
+
+
+    if( aoi == NULL && aoi->count < 3 )
+    {
+        return false;
+    }
+
+    for( i = 1; i < aoi->count; i++ )
+    {
+        if( gac_aoi_intersect( &aoi->ray_origin, &point, &aoi->points[i - 1],
+                &aoi->points[i] ) )
+        {
+            count++;
+        }
+    }
+
+    if( count % 2 == 0 )
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+}
+
+/******************************************************************************/
+bool gac_aoi_init( gac_aoi_t* aoi )
+{
+    uint32_t i;
+
+    if( aoi == NULL )
+    {
+        return false;
+    }
+
+    glm_vec2_zero( aoi->ray_origin );
+    aoi->count = 0;
+    aoi->avg_edge_len = 0;
+    aoi->_me = NULL;
+
+    for( i = 0; i < GAC_AOI_MAX_POINTS; i++ )
+    {
+        glm_vec2_zero( aoi->points[i] );
+    }
+
+    return false;
+}
+
+/******************************************************************************/
+bool gac_aoi_intersect( vec2* p1, vec2* q1, vec2* p2, vec2* q2 )
+{
+    gac_aoi_orientation_t o1 = gac_aoi_orientation_triplet( p1, q1, p2 );
+    gac_aoi_orientation_t o2 = gac_aoi_orientation_triplet( p1, q1, q2 );
+    gac_aoi_orientation_t o3 = gac_aoi_orientation_triplet( p2, q2, p1 );
+    gac_aoi_orientation_t o4 = gac_aoi_orientation_triplet( p2, q2, q1 );
+
+    // General case
+    if( o1 != o2 && o3 != o4 )
+    {
+        return true;
+    }
+
+    // Special Cases
+    if( o1 == GAC_AOI_ORIENTATION_COLINEAR
+            && gac_aoi_point_on_segment( p1, p2, q1 ) )
+    {
+        // p1, q1 and p2 are collinear and p2 lies on segment p1q1
+        return true;
+    }
+
+    if( o2 == GAC_AOI_ORIENTATION_COLINEAR
+            && gac_aoi_point_on_segment( p1, q2, q1 ) )
+    {
+        // p1, q1 and q2 are collinear and q2 lies on segment p1q1
+        return true;
+    }
+
+    if( o3 == GAC_AOI_ORIENTATION_COLINEAR
+            && gac_aoi_point_on_segment( p2, p1, q2 ) )
+    {
+        // p2, q2 and p1 are collinear and p1 lies on segment p2q2
+        return true;
+    }
+
+    if( o4 == GAC_AOI_ORIENTATION_COLINEAR
+            && gac_aoi_point_on_segment( p2, q1, q2 ) )
+    {
+        // p2, q2 and q1 are collinear and q1 lies on segment p2q2
+        return true;
+    }
+
+    return false;
+}
+
+/******************************************************************************/
+bool gac_aoi_point_on_segment( vec2* p, vec2* s1, vec2* s2 )
+{
+    if( ( *p )[0] <= fmax( ( *s1 )[0], ( *s2 )[0] )
+            && ( *p )[0] >= fmin( ( *s1 )[0], ( *s2 )[0] )
+            && ( *p )[1] <= fmax( ( *s1 )[1], ( *s2 )[1] )
+            && ( *p )[1] >= fmin( ( *s1 )[1], ( *s2 )[1] ) )
+    {
+        return true;
+    }
+
+    return false;
+}
+
+/******************************************************************************/
+gac_aoi_orientation_t gac_aoi_orientation_triplet( vec2* p, vec2* q, vec2* r )
+{
+    uint32_t val = ( ( *q )[1] - ( *p )[1] ) * ( ( *r )[0] - ( *q )[0] ) -
+            ( ( *q )[0] - ( *p )[0]) * ( ( *r )[1] - ( *q )[1] );
+
+    if( val == 0 )
+    {
+        return GAC_AOI_ORIENTATION_COLINEAR;
+    }
+    else if( val > 0 )
+    {
+        return GAC_AOI_ORIENTATION_CLOCKWISE;
+    }
+    else
+    {
+        return GAC_AOI_ORIENTATION_COUNTER_CLOCKWISE;
+    }
+}
+
+/******************************************************************************/
 bool gac_filter_fixation( gac_filter_fixation_t* filter, gac_sample_t* sample,
         gac_fixation_t* fixation )
 {
